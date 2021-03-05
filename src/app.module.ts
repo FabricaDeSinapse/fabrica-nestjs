a2fdfb1273c3566fc0e3f889ccfcf66ed399d83d
Paulo Salvatore <salvatoregames@gmail.com>
2021-03-05 19:36:57 -0300

    chore: add messages controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MessagesController } from "./messages/messages.controller";
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MessagesController],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,5 @@
+import { Controller } from "@nestjs/common";
+
+@Controller("messages")
+export class MessagesController {
+}
