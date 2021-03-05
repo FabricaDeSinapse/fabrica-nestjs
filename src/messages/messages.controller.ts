3cd1d47623999735f45896931007215b5d0be5bc
Paulo Salvatore <salvatoregames@gmail.com>
2021-03-05 20:25:19 -0300

    chore: implement ParseIntPipe

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   NotFoundException,
   Param,
+  ParseIntPipe,
   Post,
   Put,
 } from '@nestjs/common';
@@ -21,8 +22,8 @@ export class MessagesController {
   }
 
   @Get(':id')
-  findById(@Param() params) {
-    return this.messagesService.findById(+params.id).catch((e) => {
+  findById(@Param('id', ParseIntPipe) id) {
+    return this.messagesService.findById(id).catch((e) => {
       throw new NotFoundException(e.message);
     });
   }
@@ -33,15 +34,15 @@ export class MessagesController {
   }
 
   @Put(':id')
-  update(@Param() params, @Body() messageDto: MessageDto) {
-    return this.messagesService.update(+params.id, messageDto).catch((e) => {
+  update(@Param('id', ParseIntPipe) id, @Body() messageDto: MessageDto) {
+    return this.messagesService.update(id, messageDto).catch((e) => {
       throw new NotFoundException(e.message);
     });
   }
 
   @Delete(':id')
-  delete(@Param() params) {
-    return this.messagesService.delete(+params.id).catch((e) => {
+  delete(@Param('id', ParseIntPipe) id) {
+    return this.messagesService.delete(id).catch((e) => {
       throw new NotFoundException(e.message);
     });
   }
